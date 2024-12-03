242bf71525fc4d9f0f1c715ba155bb11a74a6384
shz-code <web.shahidul.alam@gmail.com>
2024-12-03 11:16:06 +0600

    add: Client init (native wind)

---
+++ client/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Index = () => {
+  return (
+    <View className="h-full items-center justify-center">
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+};
+
+export default Index;
